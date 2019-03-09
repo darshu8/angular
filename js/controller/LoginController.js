5306295794687d9d68d0c5b326cf3af637232c50
darshu8 <nnd.darshan@gmail.com>
2019-03-09 21:39:07 +0530

    included controller in index.html

--- js/controller/LoginController.js
+++ js/controller/LoginController.js
@@ -1,4 +1,17 @@
-app.controller('LoginController', ['$scope',function(Scope){
+app.controller('LoginController', ['$scope','$location',function(Scope,location){
 	console.log("hello world");
 	Scope.msg="hello from login";
+	Scope.login = function(){
+		var obj={};
+		obj.username = Scope.username;
+		obj.password = Scope.password;
+		if(obj.username == "admin" && obj.password == "admin"){
+			var user = JSON.stringify(obj);
+			localStorage.setItem("user",user);
+			location.path("/home");
+		}
+		else{
+			Scope.errMsg="Invalid username/password";
+		}		
+	}
 }]); 
